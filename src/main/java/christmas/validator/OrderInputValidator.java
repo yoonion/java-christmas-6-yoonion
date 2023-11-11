e14efbf2ae93618fe4b92d2b90d4340f50e7d7cf
yoonion <yoonion97@gmail.com>
2023-11-11 19:41:11 +0900

    refactor: 주문 메뉴 형식, 주문 개수 체크 메서드 중복 코드 제거

--- src/main/java/christmas/validator/OrderInputValidator.java
+++ src/main/java/christmas/validator/OrderInputValidator.java
@@ -1,39 +1,35 @@
 package christmas.validator;
 
+import java.util.Arrays;
+
 public class OrderInputValidator {
 
     private final static String DEFAULT_ERROR_MESSAGE = ValidatorConstants.DEFAULT_ERROR_MESSAGE;
     private final static String INVALID_ORDER_MESSAGE = DEFAULT_ERROR_MESSAGE + "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private final static String ORDER_DELIMITER = ",";
+    private final static String MENU_AND_QUANTITY_DELIMITER = "-";
 
     private final static int ORDER_MINIMUM_QUANTITY = 1;
     private final static int ORDER_MAXIMUM_QUANTITY = 20;
 
     public void checkOrderFormat(String inputOrderMenuAndQuantity) {
         checkContainHyphen(inputOrderMenuAndQuantity);
-        if (isContainComma(inputOrderMenuAndQuantity)) {
-            String[] orderMenuAndQuantities = inputOrderMenuAndQuantity.split(",");
-            int totalOrderQuantity = 0;
-            for (String orderMenuAndQuantity : orderMenuAndQuantities) {
-                String orderMenu = orderMenuAndQuantity.split("-")[0];
-                String orderQuantity = orderMenuAndQuantity.split("-")[1];
-                checkOrderQuantityRange(orderQuantity);
-                totalOrderQuantity += Integer.parseInt(orderQuantity);
-            }
-            checkTotalOrderQuantity(totalOrderQuantity);
+
+        int totalOrderQuantity = 0;
+        String[] orderMenuAndQuantities = inputOrderMenuAndQuantity.split(ORDER_DELIMITER);
+        for (String orderMenuAndQuantity : orderMenuAndQuantities) {
+            // String orderMenu = orderMenuAndQuantity.split(MENU_AND_QUANTITY_DELIMITER)[0];
+            String orderQuantity = orderMenuAndQuantity.split(MENU_AND_QUANTITY_DELIMITER)[1];
+            checkOrderQuantityRange(orderQuantity);
+            totalOrderQuantity += Integer.parseInt(orderQuantity);
         }
+        checkTotalOrderQuantity(totalOrderQuantity);
     }
 
     private void checkContainHyphen(String inputOrderMenuAndQuantity) {
-        if (!inputOrderMenuAndQuantity.contains("-")) {
+        if (!inputOrderMenuAndQuantity.contains(MENU_AND_QUANTITY_DELIMITER)) {
             throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
-    }
-
-    private boolean isContainComma(String inputOrderMenuAndQuantity) {
-        if (inputOrderMenuAndQuantity.contains(",")) {
-            return true;
-        }
-        return false;
     }
 
     private void checkOrderQuantityRange(String orderQuantity) {
@@ -48,7 +44,7 @@ public class OrderInputValidator {
     }
 
     private void checkTotalOrderQuantity(int totalOrderQuantity) {
-        if (totalOrderQuantity > ORDER_MAXIMUM_QUANTITY) {
+        if (totalOrderQuantity > ORDER_MAXIMUM_QUANTITY || totalOrderQuantity < ORDER_MINIMUM_QUANTITY) {
             throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
     }
