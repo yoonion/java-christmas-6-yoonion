88e551fa462889278a33440fa6e19bebb9d682c9
yoonion <yoonion97@gmail.com>
2023-11-11 16:40:59 +0900

    feat: 방문 날짜, 주문 메뉴 검증 클래스 스켈레톤 코드 작성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,13 +1,19 @@
 package christmas;
 
 import christmas.controller.ChristmasController;
+import christmas.validator.OrderInputValidator;
+import christmas.validator.VisitDateInputValidator;
 import christmas.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        VisitDateInputValidator visitDateInputValidator = new VisitDateInputValidator();
+        OrderInputValidator orderInputValidator = new OrderInputValidator();
 
-        ChristmasController christmasController = new ChristmasController(inputView);
+        ChristmasController christmasController = new ChristmasController(
+                inputView, visitDateInputValidator, orderInputValidator
+        );
         christmasController.promotionRun();
     }
 }
--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,15 +1,26 @@
 package christmas.controller;
 
+import christmas.validator.OrderInputValidator;
+import christmas.validator.VisitDateInputValidator;
 import christmas.view.InputView;
 
 public class ChristmasController {
 
     private final InputView inputView;
+    private final VisitDateInputValidator visitDateInputValidator;
+    private final OrderInputValidator orderInputValidator;
 
-    public ChristmasController(InputView inputView) {
+    public ChristmasController(
+            InputView inputView,
+            VisitDateInputValidator visitDateInputValidator,
+            OrderInputValidator orderInputValidator
+    ) {
         this.inputView = inputView;
+        this.visitDateInputValidator = visitDateInputValidator;
+        this.orderInputValidator = orderInputValidator;
     }
 
+    // 게임 진행 총괄 메서드
     public void promotionRun() {
         inputView.inputVisitDate();
         inputView.inputOrderMenuAndQuantity();
