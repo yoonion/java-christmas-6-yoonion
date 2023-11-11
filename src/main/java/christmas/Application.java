f8f246bbebf048683fa38ff2a822ed43a451f5a9
yoonion <yoonion97@gmail.com>
2023-11-11 16:13:08 +0900

    feat: ChristmasController 의존성 주입 및 게임 총괄 진행 메서드 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.controller.ChristmasController;
+import christmas.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+
+        ChristmasController christmasController = new ChristmasController(inputView);
+        christmasController.promotionRun();
     }
 }
--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,5 +1,18 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+
 public class ChristmasController {
+
+    private final InputView inputView;
+
+    public ChristmasController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void promotionRun() {
+        inputView.inputVisitDate();
+        inputView.inputOrderMenuAndQuantity();
+    }
 
 }
