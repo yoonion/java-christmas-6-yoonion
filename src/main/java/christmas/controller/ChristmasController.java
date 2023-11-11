00469be78955b7ea9fb24f804778ea85c1043bf8
yoonion <yoonion97@gmail.com>
2023-11-11 16:07:56 +0900

    feat: 주문 메뉴 입력받는 메서드 구현

---
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -0,0 +1,5 @@
+package christmas.controller;
+
+public class ChristmasController {
+
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,10 +6,16 @@ public class InputView {
 
     private static final String INTRODUCTION_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String VISIT_DATE_QUESTION_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public String inputVisitDate() {
         System.out.println(INTRODUCTION_MESSAGE);
         System.out.println(VISIT_DATE_QUESTION_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputOrderMenuAndQuantity() {
+        System.out.println(ORDER_MENU_MESSAGE);
         return Console.readLine();
     }
 }
