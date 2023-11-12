8674a3c3f8d1a1a3f077a93fbdfe08a58bd4c34f
yoonion <yoonion97@gmail.com>
2023-11-12 15:28:23 +0900

    feat: ChristmasController에 OutputView 클래스 의존성 주입

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -4,15 +4,17 @@ import christmas.controller.ChristmasController;
 import christmas.validator.OrderInputValidator;
 import christmas.validator.VisitDateInputValidator;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
         VisitDateInputValidator visitDateInputValidator = new VisitDateInputValidator();
         OrderInputValidator orderInputValidator = new OrderInputValidator();
 
         ChristmasController christmasController = new ChristmasController(
-                inputView, visitDateInputValidator, orderInputValidator
+                inputView, outputView, visitDateInputValidator, orderInputValidator
         );
         christmasController.promotionRun();
     }
--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -3,19 +3,23 @@ package christmas.controller;
 import christmas.validator.OrderInputValidator;
 import christmas.validator.VisitDateInputValidator;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class ChristmasController {
 
     private final InputView inputView;
+    private final OutputView outputView;
     private final VisitDateInputValidator visitDateInputValidator;
     private final OrderInputValidator orderInputValidator;
 
     public ChristmasController(
             InputView inputView,
+            OutputView outputView,
             VisitDateInputValidator visitDateInputValidator,
             OrderInputValidator orderInputValidator
     ) {
         this.inputView = inputView;
+        this.outputView = outputView;
         this.visitDateInputValidator = visitDateInputValidator;
         this.orderInputValidator = orderInputValidator;
     }
