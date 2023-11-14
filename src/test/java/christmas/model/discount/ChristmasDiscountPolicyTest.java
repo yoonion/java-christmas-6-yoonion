b85f48f37d325513fe92b74dec34e1922d5a5f08
yoonion <yoonion97@gmail.com>
2023-11-14 19:32:53 +0900

    refactor: 크리스마스 디데이 할인 테스트 코드 중복 매개변수 @MethodSource로 변경

--- src/test/java/christmas/model/discount/ChristmasDiscountPolicyTest.java
+++ src/test/java/christmas/model/discount/ChristmasDiscountPolicyTest.java
@@ -5,17 +5,36 @@ import christmas.service.ChristmasService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ChristmasDiscountPolicyTest {
 
     ChristmasDiscountPolicy christmasDiscountPolicy = new ChristmasDiscountPolicy();
     ChristmasService christmasService = new ChristmasService();
 
+    // 크리스마스 디데이 할인에 포함된 날짜
+    static Stream<Arguments> christmasDiscountDayProvider(){
+        return Stream.of(
+                arguments(1), arguments(2), arguments(7), arguments(15), arguments(20), arguments(25)
+        );
+    }
+
+    // 크리스마스 디데이 할인이 아닌 날짜
+    static Stream<Arguments> notChristmasDiscountDayProvider(){
+        return Stream.of(
+                arguments(26), arguments(27), arguments(28), arguments(29), arguments(30), arguments(31)
+        );
+    }
+
     @DisplayName("주문 금액이 10,000원 이상 일 때 크리스마스 할인 정책 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 7, 15, 20, 25})
+    @MethodSource("christmasDiscountDayProvider")
     void christmasDiscountPriceTest(int visitDate) {
         int expectPrice = 1000 + ((visitDate - 1) * 100); // 1일 1,000원 2일 1,100원 ... 1일 증가할 때마다 100원씩 증가
         Orders orders = christmasService.createOrders("티본스테이크-1");
@@ -26,7 +45,7 @@ class ChristmasDiscountPolicyTest {
 
     @DisplayName("주문 금액이 10,000원 미만 일 때 크리스마스 할인 정책 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 7, 15, 20, 25})
+    @MethodSource("christmasDiscountDayProvider")
     void christmasNotDiscountPriceTest(int visitDate) {
         Orders orders = christmasService.createOrders("양송이수프-1");
         int discountPrice = christmasDiscountPolicy.applyChristmasDiscountPrice(visitDate, orders);
@@ -36,7 +55,7 @@ class ChristmasDiscountPolicyTest {
 
     @DisplayName("이벤트 날짜가 아닐 때 크리스마스 할인 정책 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {26, 27, 28, 29, 30, 31})
+    @MethodSource("notChristmasDiscountDayProvider")
     void notRangeChristmasEventDateTest(int visitDate) {
         Orders orders = christmasService.createOrders("티본스테이크-1");
         int discountPrice = christmasDiscountPolicy.applyChristmasDiscountPrice(visitDate, orders);
